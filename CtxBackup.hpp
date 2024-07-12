8d97f258f4699365c014708e63242665d31c12ff
Leopard20 <16115820+Leopard20@users.noreply.github.com>
2024-07-12 19:59:30 +0330

    Added helper class for backing up device context parameters

---
+++ CtxBackup.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "DxDef.hpp"
+namespace DX11
+{
+	class ContextBackup
+	{
+		ID3D11DeviceContext* ctx;
+	public:
+		ContextBackup(ID3D11DeviceContext* ctx);
+		~ContextBackup();
+	protected:
+		static void Backup(ID3D11DeviceContext* ctx);
+		static void Restore(ID3D11DeviceContext* ctx);
+	};
+}
---
+++ DxImp.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <d3d11.h>       // D3D interface
+#include <dxgi.h>        // DirectX driver interface
+#include <d3dcompiler.h> // shader compiler
+#pragma comment( lib, "user32" )          // link against the win32 library
+#pragma comment( lib, "d3d11.lib" )       // direct3D library
+#pragma comment( lib, "dxgi.lib" )        // directx graphics interface
+#pragma comment( lib, "d3dcompiler.lib" ) // shader compiler
